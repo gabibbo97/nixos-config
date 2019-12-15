14768bae7ec2ea39ec6ec206608a5ca74a9d430f
Giacomo Longo <gabibbo97@gmail.com>
2019-12-15 23:08:36 +0100

    Add special lenovo buttons

--- hosts/giacomo-thinkpad.nix
+++ hosts/giacomo-thinkpad.nix
@@ -11,6 +11,9 @@
       XF86Search = "exec $menu";
       XF86LaunchA = "exec $menu";
       XF86Explorer = "exec $menu";
+      XF86Tools = "exec notify-send --expire-time=1500 'Unsupported key' 'No action mapped to tools button'";
+      XF86Display = "exec notify-send --expire-time=1500 'Unsupported key' 'No action mapped to display button'";
+      XF86WLAN = "exec notify-send --expire-time=1500 'Network card switched' 'Pressed WLAN key'";
     };
     extraInputConfig = ''
       input "1739:0:Synaptics_tm2963-001" {
